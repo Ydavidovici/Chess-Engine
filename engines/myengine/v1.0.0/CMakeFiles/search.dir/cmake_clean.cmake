24465c1407815c7f688cc9d1e39b35bba75e85db
Ydavidovici <150648286+Ydavidovici@users.noreply.github.com>
2025-07-16 13:49:05 -0400

    Add myengine build v1.0.0

---
+++ engines/myengine/v1.0.0/CMakeFiles/search.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/search.dir/src/search.cpp.o"
+  "CMakeFiles/search.dir/src/search.cpp.o.d"
+  "CMakeFiles/search.dir/src/timeman.cpp.o"
+  "CMakeFiles/search.dir/src/timeman.cpp.o.d"
+  "libsearch.a"
+  "libsearch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ engines/myengine/v1.0.0/CMakeFiles/test_eval.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_eval.dir/Users/yaakovdavidovici/Coding/Chess-Engine/tests/myengine/eval_test.cpp.o"
+  "CMakeFiles/test_eval.dir/Users/yaakovdavidovici/Coding/Chess-Engine/tests/myengine/eval_test.cpp.o.d"
+  "test_eval"
+  "test_eval.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_eval.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
