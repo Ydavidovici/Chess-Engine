dd2a0f2d73fe3f834f8dce73a47318350e5eb28e
Ydavidovici <150648286+Ydavidovici@users.noreply.github.com>
2025-07-16 13:59:26 -0400

    Add myengine build v1.0.1

---
+++ engines/myengine/builds/v1.0.1/CMakeFiles/chessengine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chessengine.dir/src/bindings.cpp.o"
+  "CMakeFiles/chessengine.dir/src/bindings.cpp.o.d"
+  "chessengine.cpython-313-darwin.so"
+  "chessengine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chessengine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ engines/myengine/builds/v1.0.1/CMakeFiles/test_tt.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for test_tt.
+# This may be replaced when dependencies are built.
