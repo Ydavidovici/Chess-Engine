f2966cc55a89d7ef4f642b8959a77e421244ed50
Ydavidovici <150648286+Ydavidovici@users.noreply.github.com>
2025-07-16 13:47:29 -0400

    Add myengine build v1.0.0

---
+++ engines/myengine/engines/myengine/v1.0.0/CMakeFiles/test_search.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_search.dir/Users/yaakovdavidovici/Coding/Chess-Engine/tests/myengine/search_test.cpp.o"
+  "CMakeFiles/test_search.dir/Users/yaakovdavidovici/Coding/Chess-Engine/tests/myengine/search_test.cpp.o.d"
+  "test_search"
+  "test_search.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ engines/myengine/engines/myengine/v1.0.0/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine
+# Build directory: /Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_board_move "/Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/build/test_board_move")
+set_tests_properties(test_board_move PROPERTIES  _BACKTRACE_TRIPLES "/Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/CMakeLists.txt;58;add_test;/Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/CMakeLists.txt;0;")
+add_test(test_eval "/Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/build/test_eval")
+set_tests_properties(test_eval PROPERTIES  _BACKTRACE_TRIPLES "/Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/CMakeLists.txt;58;add_test;/Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/CMakeLists.txt;0;")
+add_test(test_search "/Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/build/test_search")
+set_tests_properties(test_search PROPERTIES  _BACKTRACE_TRIPLES "/Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/CMakeLists.txt;58;add_test;/Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/CMakeLists.txt;0;")
+add_test(test_tt "/Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/build/test_tt")
+set_tests_properties(test_tt PROPERTIES  _BACKTRACE_TRIPLES "/Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/CMakeLists.txt;58;add_test;/Users/yaakovdavidovici/Coding/Chess-Engine/engines/myengine/CMakeLists.txt;0;")
